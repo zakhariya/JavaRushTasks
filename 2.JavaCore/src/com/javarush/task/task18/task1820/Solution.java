478df57fb67d14738a4c098a590ebf66b9c551b2
zakhariya <alexander.zakhariya@gmail.com>
2019-09-26 17:31:07 +0300

    updated from work

--- 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -4,8 +4,24 @@ package com.javarush.task.task18.task1820;
 Округление чисел
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        InputStream inputStream = new FileInputStream(reader.readLine());
+        OutputStream outputStream = new FileOutputStream(reader.readLine());
+
+        byte[] bytes = new byte[inputStream.available()];
+
+        inputStream.read(bytes);
+
+        String s = new String(bytes, "windows-1251");
+
+        System.out.println(s);
+
+//        System.out.println(new DecimalFormat("#0.00").format(result));
 
     }
 }
