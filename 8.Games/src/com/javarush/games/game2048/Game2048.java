7cfed96093caff06e44b1c6ab2a7e1fc4f70fbeb
zakhariya <alexander.zakhariya@gmail.com>
2019-12-12 09:00:19 +0200

    updated from work

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -3,4 +3,41 @@ package com.javarush.games.game2048;
 import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
+    private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
+        drawScene();
+    }
+
+    private void createGame() {
+        createNewNumber();
+        createNewNumber();
+    }
+
+    private void drawScene() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                setCellColor(j, i, Color.WHITE);
+            }
+        }
+    }
+
+    private void createNewNumber() {
+        while (true) {
+            int x = getRandomNumber(SIDE);
+            int y = getRandomNumber(SIDE);
+
+            if (gameField[x][y] == 0) {
+                int value = getRandomNumber(10);
+                System.out.println(value);
+                gameField[x][y] = value;
+
+                break;
+            }
+        }
+    }
 }
