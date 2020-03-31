f0d515307051be652143f4fef78024098ff57309
zakhariya <alexander.zakhariya@gmail.com>
2020-03-31 11:36:01 +0300

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task22.task2208;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /* 
@@ -7,10 +9,29 @@ import java.util.Map;
 */
 public class Solution {
     public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
 
+        map.put("name", "Ivanov");
+        map.put("country", "Ukraine");
+        map.put("city", "Kiev");
+        map.put("age", null);
+
+        System.out.println(getQuery(map));
     }
 
     public static String getQuery(Map<String, String> params) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if(Collections.frequency(params.values(), null) == params.size()) {
+            return "";
+        }
+
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getValue() != null) {
+                stringBuilder.append(pair.getKey() + " = \'"+ pair.getValue() + "\' and ");
+            }
+        }
+
+        return stringBuilder.toString().substring(0, stringBuilder.lastIndexOf(" and "));
     }
 }
