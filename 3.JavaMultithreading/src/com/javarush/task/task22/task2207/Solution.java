2e2614dcff7953d715a629c75e8acf2f69767263
zakhariya <alexander.zakhariya@gmail.com>
2020-03-31 11:22:43 +0300

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task22.task2207;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,8 +12,39 @@ import java.util.List;
 public class Solution {
     public static List<Pair> result = new LinkedList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
+        String fileName = consoleReader.readLine();
+
+        BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF8"));
+
+        List<String> words = new ArrayList<>();
+
+        while (fileReader.ready()) {
+            words.addAll(Arrays.asList(fileReader.readLine().split(" ")));
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = i + 1; j < words.size(); j++) {
+                if (words.get(i).equals(new StringBuilder(words.get(j)).reverse().toString())) {
+                    Pair pair = new Pair();
+
+                    pair.first = words.get(i);
+                    pair.second = words.get(j);
+
+                    result.add(pair);
+
+                    words.remove(j);
+
+                    break;
+                }
+            }
+        }
+
+        for (Pair pair : result) {
+            System.out.println(pair);
+        }
     }
 
     public static class Pair {
@@ -25,8 +59,8 @@ public class Solution {
             Pair pair = (Pair) o;
 
             if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
-            return second != null ? second.equals(pair.second) : pair.second == null;
 
+            return second != null ? second.equals(pair.second) : pair.second == null;
         }
 
         @Override
