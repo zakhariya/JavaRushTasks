6d4128629c7aa7f1914c18a996e964619bcc7a17
zakhariya <alexander.zakhariya@gmail.com>
2020-04-24 19:13:52 +0300

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -1,18 +1,23 @@
 package com.javarush.task.task22.task2212;
 
-/* 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
 Проверка номера телефона
 */
 public class Solution {
+    private static final String REGEX = "^(\\+\\d{2})?(\\(\\d{3}\\)|\\d{3})(?!(?:.*\\-){3})(?!.*\\-\\-)(?:[^\\d()]*?\\d){7}$";
+
     public static boolean checkTelNumber(String telNumber) {
         if (telNumber == null) {
             return false;
         }
 
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(telNumber);
 
-        //        telNumber.matches();
-
-        return false;
+        return matcher.matches();
     }
 
     public static void main(String[] args) {
