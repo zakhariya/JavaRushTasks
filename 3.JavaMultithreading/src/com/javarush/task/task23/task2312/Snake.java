c0ed34276ab3da1fb12088a05c6ff1ea1797846d
zakhariya <alexander.zakhariya@gmail.com>
2020-04-30 09:45:06 +0300

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,57 +1,24 @@
 package com.javarush.task.task23.task2312;
 
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Класс змея
+ */
 public class Snake {
-    private List<SnakeSection> sections;
-    private boolean isAlive;
+
     private SnakeDirection direction;
+    private boolean isAlive;
+    private ArrayList<SnakeSection> sections;
 
     public Snake(int x, int y) {
-        sections = new ArrayList<>();
-
-        SnakeSection head = new SnakeSection(x, y);
-        sections.add(head);
-
+        sections = new ArrayList<SnakeSection>();
+        sections.add(new SnakeSection(x, y));
         isAlive = true;
     }
 
-    public void move() {
-        if (!isAlive) {
-            return;
-        }
-
-        if (direction == SnakeDirection.UP) {
-            move(0, -1);
-        } else if (direction == SnakeDirection.RIGHT) {
-            move(1, 0);
-        } else if (direction == SnakeDirection.DOWN) {
-            move(0, 1);
-        } else if (direction == SnakeDirection.LEFT) {
-            move(-1, 0);
-        }
-    }
-
-    public void move(int x, int y) {
-
-    }
-
-    public void checkBorders(SnakeSection head) {
-        int x = head.getX();
-        int y = head.getY();
-
-        int width = Room.game.getWidth();
-        int height = Room.game.getHeight();
-
-        if ((x < 0 || x >= width)
-                || (y < 0 || y >= height)) {
-            isAlive = false;
-        }
-    }
-
-    public void checkBody(SnakeSection head) {
-
+    public boolean isAlive() {
+        return isAlive;
     }
 
     public int getX() {
@@ -62,19 +29,64 @@ public class Snake {
         return sections.get(0).getY();
     }
 
-    public List<SnakeSection> getSections() {
-        return sections;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
     public SnakeDirection getDirection() {
         return direction;
     }
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
+    }
+
+    public ArrayList<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public void move() {
+        if (!isAlive) return;
+
+        if (direction == SnakeDirection.UP)
+            move(0, -1);
+        else if (direction == SnakeDirection.RIGHT)
+            move(1, 0);
+        else if (direction == SnakeDirection.DOWN)
+            move(0, 1);
+        else if (direction == SnakeDirection.LEFT)
+            move(-1, 0);
+    }
+
+
+    private void move(int dx, int dy) {
+        SnakeSection head = sections.get(0);
+        head = new SnakeSection(head.getX() + dx, head.getY() + dy);
+
+        checkBorders(head);
+        if (!isAlive) return;
+
+        checkBody(head);
+        if (!isAlive) return;
+
+        Mouse mouse = Room.game.getMouse();
+
+        if (head.getX() == mouse.getX() && head.getY() == mouse.getY()) {
+            sections.add(0, head);
+            Room.game.eatMouse();
+        } else {
+            sections.add(0, head);
+            sections.remove(sections.size() - 1);
+        }
+    }
+
+    private void checkBorders(SnakeSection head) {
+        if ((head.getX() < 0 || head.getX() >= Room.game.getWidth())
+                || head.getY() < 0 || head.getY() >= Room.game.getHeight()) {
+
+            isAlive = false;
+        }
+    }
+
+    private void checkBody(SnakeSection head) {
+        if (sections.contains(head)) {
+            isAlive = false;
+        }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task23.task2312;
 
-import java.util.Objects;
-
 public class SnakeSection {
     private int x;
     private int y;
+
 
     public SnakeSection(int x, int y) {
         this.x = x;
@@ -22,15 +21,20 @@ public class SnakeSection {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof SnakeSection)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+
         SnakeSection that = (SnakeSection) o;
-        return x == that.x &&
-                y == that.y;
+
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(x, y);
+        int result = x;
+        result = 31 * result + y;
+        return result;
     }
 }
