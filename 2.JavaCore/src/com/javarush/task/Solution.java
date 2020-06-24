325626a26b3c2f493c2c63b8b50df91377554061
zakhariya <alexander.zakhariya@gmail.com>
2020-06-24 17:31:20 +0300

    updated from home

--- 2.JavaCore/src/com/javarush/task/Solution.java
+++ 2.JavaCore/src/com/javarush/task/Solution.java
@@ -1,37 +1,15 @@
 package com.javarush.task;
 
-import java.io.*;
-
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //кладем данные в строку
-        StringBuilder sb = new StringBuilder();
-        sb.append("Lena").append('\n');
-        sb.append("Olya").append('\n');
-        sb.append("Anya").append('\n');
-        String data = sb.toString();
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Bu");
+            }
+        };
 
-        //Оборачиваем строку в класс ByteArrayInputStream
-        InputStream is = new ByteArrayInputStream(data.getBytes());
-
-        //подменяем in
-        System.setIn(is);
-
-
-        //вызываем обычный метод, который не подозревает о наших манипуляциях
-        readAndPrintLine();
+        thread.start();
     }
 
-    public static void readAndPrintLine() throws IOException
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true)
-        {
-            String line = reader.readLine();
-            if (line == null) break;
-            System.out.println(line);
-        }
-        reader.close();
-    }
 }
