018807419d4beaa715904b4384b5207411b0ee79
zakhariya <alexander.zakhariya@gmail.com>
2020-11-01 19:58:55 +0200

    updated from home

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2405/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task24.task2405;
+
+/* 
+Black box
+*/
+
+public class Solution implements Action {
+    public static int countActionObjects;
+
+    private int param;
+
+    public Solution(int param) {
+        this.param = param;
+    }
+
+    private Action solutionAction = new Action() {
+
+        @Override
+        public void someAction() {
+
+            Action firstAction = new FirstClass() {
+                @Override
+                public Action getDependantAction() {
+                    return null;
+                }
+            };
+
+            Action secondAction = new SecondClass();
+
+            if (param > 0) {
+                for (int i = 5; i > 0; i--) {
+                    System.out.println(i);
+                }
+
+                firstAction.someAction();
+            } else {
+                secondAction.someAction();
+            }
+        }
+    };
+
+
+    @Override
+    public void someAction() {
+        solutionAction.someAction();
+    }
+
+    /**
+     * 5
+     * 4
+     * 3
+     * 2
+     * 1
+     * class FirstClass, method someAction
+     * class SecondClass, method someAction
+     * Specific action for anonymous SecondClass, param = 0
+     * Count of created Action objects is 2
+     * class SecondClass, method someAction
+     * Specific action for anonymous SecondClass, param = -1
+     * Count of created Action objects is 3
+     */
+    public static void main(String[] args) {
+        Solution solution = new Solution(5);
+        solution.someAction();
+        System.out.println("Count of created Action objects is " + countActionObjects);
+
+        solution = new Solution(-1);
+        solution.someAction();
+        System.out.println("Count of created Action objects is " + countActionObjects);
+    }
+}
