4193fb15ac8c4882fa56561e3c653ddef7d4448d
zakhariya <alexander.zakhariya@gmail.com>
2020-11-11 18:50:03 +0200

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -2,7 +2,7 @@ package com.javarush.task.task29.task2909.car;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
     static public final int TRUCK = 0;
     static public final int SEDAN = 1;
     static public final int CABRIOLET = 2;
@@ -18,39 +18,58 @@ public class Car {
     private boolean driverAvailable;
     private int numberOfPassengers;
 
-    public Car(int type, int numberOfPassengers) {
+    protected Car(int type, int numberOfPassengers) {
         this.type = type;
         this.numberOfPassengers = numberOfPassengers;
     }
 
-    public int fill(double numberOfLiters) {
-        if (numberOfLiters < 0)
-            return -1;
+    public static Car create(int type, int numberOfPassengers) {
+        switch (type) {
+            case TRUCK:
+                return new Truck(numberOfPassengers);
+            case SEDAN:
+                return new Sedan(numberOfPassengers);
+            case CABRIOLET:
+                return new Cabriolet(numberOfPassengers);
+            default:
+                return null;
+        }
+    }
+
+    public void fill(double numberOfLiters) throws Exception {
+        if (numberOfLiters < 0) {
+            throw new Exception();
+        }
+
         fuel += numberOfLiters;
-        return 0;
+    }
+
+    public double getWinterConsumption(int length) {
+        return length * winterFuelConsumption + winterWarmingUp;
+    }
+
+    public double getSummerConsumption(int length) {
+        return length * summerFuelConsumption;
     }
 
     public double getTripConsumption(Date date, int length, Date SummerStart, Date SummerEnd) {
-        double consumption;
-        if (date.before(SummerStart) || date.after(SummerEnd)) {
-            consumption = length * winterFuelConsumption + winterWarmingUp;
-        } else {
-            consumption = length * summerFuelConsumption;
-        }
-        return consumption;
+        return isSummer(date, SummerStart, SummerEnd) ? getSummerConsumption(length) : getWinterConsumption(length);
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
-        if (!isDriverAvailable())
+        if (!canPassengersBeTransferred()) {
             return 0;
-        if (fuel <= 0)
-            return 0;
+        }
 
         return numberOfPassengers;
     }
 
     public boolean isDriverAvailable() {
         return driverAvailable;
+    }
+
+    public boolean isSummer(Date date, Date summerStart, Date summerEnd) {
+        return date.after(summerStart) && date.before(summerEnd);
     }
 
     public void setDriverAvailable(boolean driverAvailable) {
@@ -60,10 +79,13 @@ public class Car {
     public void startMoving() {
         if (numberOfPassengers > 0) {
             fastenPassengersBelts();
-            fastenDriverBelt();
-        } else {
-            fastenDriverBelt();
         }
+
+        fastenDriverBelt();
+    }
+
+    private boolean canPassengersBeTransferred() {
+        return isDriverAvailable() && fuel > 0;
     }
 
     public void fastenPassengersBelts() {
@@ -72,11 +94,5 @@ public class Car {
     public void fastenDriverBelt() {
     }
 
-    public int getMaxSpeed() {
-        if (type == TRUCK)
-            return 80;
-        if (type == SEDAN)
-            return 120;
-        return 90;
-    }
+    public abstract int getMaxSpeed();
 }
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,11 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private boolean man;
 
-    private String country;
-    private String city;
-    private House house;
-
+    private Address address;
     private Work work;
 
     public User(String name, String surname, int age) {
@@ -41,24 +39,12 @@ public class User {
         this.age = age;
     }
 
-    public String getCountry() {
-        return country;
+    public boolean isMan() {
+        return man;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return country + " " + city + " " + house.house;
+    public void setMan(boolean man) {
+        this.man = man;
     }
 
     public Work getWork() {
@@ -67,5 +53,40 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public String getBoss() {
+        return work.getBoss();
+    }
+
+    public void printInfo() {
+        System.out.println("Имя: " + this.getName());
+        System.out.println("Фамилия: " + this.getSurname());
+    }
+
+    public String getAddress() {
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
+    }
+
+    public String getCountry() {
+        return address.getCountry();
+    }
+
+    public void setCountry(String country) {
+        address.setCountry(country);
+    }
+
+    public String getCity() {
+        return address.getCity();
+    }
+
+    public void setCity(String city) {
+        address.setCity(city);
+    }
+
+    public void printAdditionalInfo() {
+        String word = this.getAge() < 16 ? "моложе" : "старше";
+
+        System.out.println(String.format("Пользователь %s 16 лет", word));
     }
 }
