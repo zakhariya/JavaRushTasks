9ea7d71ed0206ce1609765c01ff7cfb043490701
zakhariya <alexander.zakhariya@gmail.com>
2020-11-23 13:53:08 +0200

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2709/TransferObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2709/TransferObject.java
@@ -5,12 +5,34 @@ public class TransferObject {
     protected volatile boolean isValuePresent = false; //use this variable
 
     public synchronized int get() {
+        while (!isValuePresent) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         System.out.println("Got: " + value);
+
+        isValuePresent = false;
+        notifyAll();
+
         return value;
     }
 
     public synchronized void put(int value) {
+        while (isValuePresent) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         this.value = value;
         System.out.println("Put: " + value);
+
+        isValuePresent = true;
+        notifyAll();
     }
 }
