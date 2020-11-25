304de922bdc9ac5dca6a2b79bf31ac60acf0c4a1
zakhariya <alexander.zakhariya@gmail.com>
2020-11-25 20:28:58 +0200

    updated from home

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -10,9 +10,19 @@ public class MailServer implements Runnable {
     @Override
     public void run() {
         long startTime = System.currentTimeMillis();
-        //сделайте что-то тут - do something here
-        String name = Thread.currentThread().getName();
-        long endTime = System.currentTimeMillis();
-        System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+
+        synchronized (mail) {
+            while (mail.getText() == null) {
+                try {
+                    mail.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String name = Thread.currentThread().getName();
+            long endTime = System.currentTimeMillis();
+            System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+        }
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source)) {
+
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+
+            try {
+
+                while (inputStream.available() > 0) {
+                    zipOutputStream.write(inputStream.read());
+                }
+            } finally {
+                inputStream.close();
+                zipOutputStream.closeEntry();
+                zipOutputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
