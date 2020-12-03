1a06e5603375eec6257a657b39a1ffda068cb723
zakhariya <alexander.zakhariya@gmail.com>
2020-12-03 19:02:21 +0200

    big task 28 lvl (3209)

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2809/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2809/Solution.java
@@ -11,11 +11,13 @@ Plants vs Zombies
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
         List<Character> characters = new ArrayList<>();
+
         characters.add(new Plant());
         characters.add(new Plant());
         characters.add(new Zombie());
         characters.add(new Zombie());
         characters.add(new Zombie());
+
         start(characters);
     }
 
@@ -26,18 +28,26 @@ public class Solution {
 
         for (final Character character : characters) {
             final String member = character.toString();
+
             System.out.println(member + " присоединился к игре");
+
             new Thread() {
                 @Override
                 public void run() {
                     System.out.println(member + " готовится играть");
+
+                    phaser.arriveAndAwaitAdvance();
+
                     if (!isEveryoneReady) {
                         isEveryoneReady = true;
                         System.out.println("Игра началась!");
                     }
+
                     character.run();
                 }
             }.start();
         }
+
+        phaser.arriveAndDeregister();
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+    View view;
+    HTMLDocument document;
+    File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
