e37664be1349e51cad7df971c6fbd8cb07659f04
zakhariya <alexander.zakhariya@gmail.com>
2020-12-07 12:21:51 +0200

    tasks 29 lvl

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task29.task2913;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 /* 
@@ -10,22 +11,22 @@ public class Solution {
     private static int numberA;
     private static int numberB;
 
-    public static String recursion(int a, int b) {
-        if (a > b) {
-            return a + " " + recursion(a - 1, b);
-        } else {
-            if (a == b) {
-                return Integer.toString(a);
-            }
-            return a + " " + recursion(a + 1, b);
-        }
-    }
-
     public static void main(String[] args) {
         Random random = new Random();
         numberA = random.nextInt(1000);
         numberB = random.nextInt(1000);
         System.out.println(getAllNumbersBetween(numberA, numberB));
         System.out.println(getAllNumbersBetween(numberB, numberA));
+    }
+
+    public static String getAllNumbersBetween(int a, int b) {
+        StringBuilder sb = new StringBuilder(a + " ");
+
+        while (a != b) {
+            sb.append(a < b ? ++a : --a);
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
     }
 }
